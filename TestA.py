d51f88808f9dee5927164dad0884d965363e8cc2
3km77 <990365112@qq.com>
2021-07-06 15:12:19 +0800

    更改ui自动化测试脚本

--- TestA.py
+++ TestA.py
@@ -1,11 +1,20 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 class TestUI():
+    def setup(self):
+        chrome_option = Options()
+        chrome_option.add_argument("--headless")
+        self.driver=webdriver.Chrome(options=chrome_option)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+    def teardown(self):
+        self.driver.quit()
     def test_a(self):
-        driver = webdriver.Chrome()
-        driver.get("https://www.baidu.com")
-        time.sleep(10)
-        driver.quit()
+        self.driver.get("https://www.baidu.com")
+        time.sleep(3)
+        assert self.driver.title=="百度一下，你就知道"
 if __name__ == '__main__':
     pytest.main()
